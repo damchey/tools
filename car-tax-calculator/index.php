41be354722e14761a3fd7a9a4f1e679b75617b64
Damchey Lhendup <damchey@hotmail.com>
2015-06-11 01:30:56 +0600

    added meta tags

--- car-tax-calculator/index.php
+++ car-tax-calculator/index.php
@@ -194,6 +194,9 @@ if (isset($_POST['tax_calculator'])) {
 <!doctype html>
 <html>
 	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<title>Bhutan Car Tax Calculator - Sales Tax and Customs Duty <?= date('Y'); ?></title>
 		<meta name="description" value="Bhutan Car Tax Calculator for revised vehicle import taxes 2014.">
 		<link rel="stylesheet" href="../css/bootstrap.min.css">
